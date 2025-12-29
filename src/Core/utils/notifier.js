1688f6553905cdeb6b980b3b554c69912a6b538c
mapman6760-dotcom <mapman6760@gmail.com>
2025-12-29 22:00:30 +0530

    updated firebase file

--- src/Core/utils/notifier.js
+++ src/Core/utils/notifier.js
@@ -1,9 +1,14 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
 const firebaseadmin = admin.initializeApp({
-    credential: admin.credential.cert(require("../../../config/mapmanFirebase.json")),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+  }),
 });
 
 
