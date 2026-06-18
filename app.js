df054e32e06040147a96ad590a923980b3904cfd
Madhubala V <madhuit21@gmail.com>
2026-06-18 19:02:55 +0530

    App link api added

--- app.js
+++ app.js
@@ -126,10 +126,10 @@ app.use(helmet());
 //Routers
 app.use("/admin", adminRouter);
 app.use("/shop", shopRouter);
-// app.get("/mapman-app-link", appController.App.mapmanAppLink);
-app.get("/mapman-app-link", (req, res) => {
-    res.sendFile(path.join(__dirname, "./appLink.html"));
-});
+app.get("/mapman-app-link", appController.App.mapmanAppLink);
+// app.get("/mapman-app-link", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./appLink.html"));
+// });
 //Check Status
 app.use('/status', async (req, res) => { res.json({ data: `${process.env.APP_NAME} API is Now Live`,image:'https://t3.ftcdn.net/jpg/06/33/54/78/360_F_633547842_AugYzexTpMJ9z1YcpTKUBoqBF0CUCk10.jpg' }) });
 
